444c968a99ccff6cd7b21afcaac81c6047d52081
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-07-17 20:29:38 -0400

    MVP

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function NasaCard({ imgURL }) {
+export default function NasaCard({ pic }, { picDate }) {
     return(
         <div className="nasa-card">
-            <img src={imgURL} alt="space is great!" style={{ maxWidth: "175px" }} />
+            <img src={pic} alt="space is great!" style={{ maxWidth: "175px" }} />
+            <p>{picDate}</p>
             <p>"So much universe and so little time." -Terry Pratchett</p>
         </div>
     );
